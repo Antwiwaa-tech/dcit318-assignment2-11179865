98dadf9d2e8b3a02faa87edd4d2e8db5ee31b95d
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-31 19:34:47 +0000

    Interface implementation illustration

---
+++ Interface Implementation/InterfaceImplementation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace InterfaceImplementation
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move() {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+
+        }
+    }
+
+public class Program
+    {
+        public static void Main(String[] args)
+        {
+            IMovable car = new Car();
+            car.Move();
+
+            IMovable bike = new Bicycle();
+            bike.Move();
+
+
+        }
+    }
+   
+}
