3522e712705b7b56a9bea349121d41710734c101
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-31 17:12:32 +0000

    inheritance and overriding

---
+++ Inheritance and Overriding/InheritanceAndOverriding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace App
+{
+    class IntheritanceandOverriding
+    {
+        class Animal
+        {
+            String sentence = "Some generic sound";
+            public virtual string MakeSound()
+            {
+                return sentence;
+            }
+        }
+
+        class Dog : Animal
+        {
+            String sentence = "Bark";
+            public override string MakeSound()
+            {
+                return sentence;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Console.WriteLine($"{dog.MakeSound()}");
+
+        }
+    }
+}
